22d8571779bdad2b1402ec3aff3e432c8d46a4d0
Dmytro <guldmytro@gmail.com>
2022-07-19 09:37:20 +0300

    added loggedout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.user_profile, name='profile'),
 ]
